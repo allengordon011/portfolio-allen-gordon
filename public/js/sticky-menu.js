1a6f843d51aeddc9052b16642632a64b793ba126
Allen Gordon <trig.code@gmail.com>
2017-05-11 16:08:28 -0500

    added styling and sticky mobile menu

--- public/js/sticky-menu.js
+++ public/js/sticky-menu.js
@@ -1,11 +1,14 @@
 var  mn = $(".menu");
+    mnm = $(".menu-media");
     mns = "menu-scrolled";
     hdr = $('home').height();
 
 $(window).scroll(function() {
   if( $(this).scrollTop() > hdr ) {
     mn.addClass(mns);
+    mnm.addClass(mns);
   } else {
     mn.removeClass(mns);
+    mnm.removeClass(mns);
   }
 });
